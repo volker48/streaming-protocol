db5576debbdb0ded29a7b0398f63e6e094f505ae
Marcus McCurdy <marcus.mccurdy@gmail.com>
2011-02-05 17:02:12 -0500

    initial commit

---
+++ protocol/src/main/java/edu/drexel/cs544/group5/server/Server.java
@@ -0,0 +1,13 @@
+package edu.drexel.cs544.group5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
