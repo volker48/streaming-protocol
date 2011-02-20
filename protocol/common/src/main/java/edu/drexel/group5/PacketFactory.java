f7c5552b2d720c91980dc89162248ea533c1439c
Marcus McCurdy <marcus.mccurdy@gmail.com>
2011-02-20 14:54:16 -0500

    added additional methods to the PacketFactory

--- protocol/common/src/main/java/edu/drexel/group5/PacketFactory.java
+++ protocol/common/src/main/java/edu/drexel/group5/PacketFactory.java
@@ -1,16 +1,83 @@
 package edu.drexel.group5;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
 
 /**
  *
  * @author Marcus McCurdy <marcus@drexel.edu>
  */
 public class PacketFactory {
+	
+	private InetSocketAddress destination;
 
-	public DatagramPacket createSessionRequest(byte protocolVersion) {
+	/**
+	 * Constructor that will set all packets created by this factory to the
+	 * destination given in the constructor.
+	 * @param destinationPort the destination port.
+	 * @param destinationAddress the destination address.
+	 */
+	public PacketFactory(int destinationPort, InetAddress destinationAddress) {
+		destination = new InetSocketAddress(destinationAddress, destinationPort);
+	}
+
+	public PacketFactory(InetSocketAddress destination) {
+		this.destination = destination;
+	}
+
+
+
+	public DatagramPacket createSessionRequest(byte protocolVersion) throws SocketException {
 		final byte[] data = new byte[]{MessageType.SESSION_REQUEST.getMessageId(), protocolVersion};
-		final DatagramPacket packet = new DatagramPacket(data, data.length);
+		final DatagramPacket packet = new DatagramPacket(data, data.length, destination);
 		return packet;
+	}
+
+	/**
+	 *
+	 * @param sessionId the id of the session.
+	 * @param version the protocol version.
+	 * @param format the String name of the stream format. Must be less than 256 characters.
+	 * @return
+	 */
+	public DatagramPacket createSessionMessage(byte sessionId, byte version, String format) throws IOException {
+		if (format.length() > 256) {
+			throw new IllegalArgumentException("Format string must be less than 256 characters!");
+		}
+		byte[] formatBytes = format.getBytes(Charset.forName("US-ASCII"));
+		final ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+		final ObjectOutputStream out = new ObjectOutputStream(bytesOut);
+		out.writeByte(MessageType.SESSION.getMessageId());
+		out.writeByte(sessionId);
+		out.writeByte(version);
+		out.writeByte(formatBytes.length);
+		out.write(formatBytes);
+		final byte[] data = bytesOut.toByteArray();
+		out.close();
+		return new DatagramPacket(data, data.length, destination);
+	}
+
+
+	public DatagramPacket createChallengeResponse(byte sessionId, int response) throws IOException {
+		final ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		final ObjectOutputStream output = new ObjectOutputStream(bytes);
+		output.writeByte(MessageType.CHALLENGE_RESPONSE.getMessageId());
+		output.writeByte(sessionId);
+		output.writeInt(response);
+		final byte[] data = bytes.toByteArray();
+		output.close();
+		return new DatagramPacket(data, data.length, destination);
+	}
+
+	public DatagramPacket createDisconnectMessage(byte sessionId) throws SocketException {
+		final byte[] data = new byte[]{MessageType.DISCONNECT.getMessageId(), sessionId};
+		return new DatagramPacket(data, data.length, destination);
 	}
 }
---
+++ protocol/common/src/test/java/edu/drexel/group5/PacketFactoryTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.drexel.group5;
+
+import java.io.PrintStream;
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+import org.junit.Test;
+
+/**
+ *
+ * @author marcus
+ */
+public class PacketFactoryTest {
+
+
+	@Test
+	public void testCreateSessionRequest() throws Exception {
+		MessageDigest md = MessageDigest.getInstance("SHA-1");
+		md.update("5".getBytes());
+		byte[] hash = md.digest();
+		System.out.println("hash = " + hash);
+		System.out.println("hash.length = " + hash.length);
+		String secret = "secret";
+		md.update("5".getBytes());
+		md.update(secret.getBytes());
+		byte[] hash2 = md.digest();
+		System.out.println("hash2: " + hash2);
+		System.out.println("hash2.length = " + hash2.length);
+		PrintStream ps = new PrintStream(System.out);
+		System.out.println("printstream hahs ourput");
+		ps.write(hash);
+		System.out.println("Hash2:");
+		ps.write(hash2);
+	}
+
+//	@Test
+//	public void testCreateSessionMessage() throws Exception {
+//	}
+//
+//	@Test
+//	public void testCreateChallengeResponse() throws Exception {
+//	}
+//
+//	@Test
+//	public void testCreateDisconnectMessage() throws Exception {
+//	}
+
+}
