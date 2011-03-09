5773417c3adcd2ab1ae3bc8d599a1cad8e7da3d0
jasonlee <devnull@localhost>
2011-03-08 22:18:21 -0600

    Added throttle, changed rate by a rate increase. bytesPerMessage and sleep balance each other out, so this doesn't do much.

--- protocol/client/src/main/java/edu/drexel/group5/protocol/Client.java
+++ protocol/client/src/main/java/edu/drexel/group5/protocol/Client.java
@@ -44,7 +44,6 @@ public class Client extends Thread {
 	private Thread playerThread;
 	private boolean isPaused = false;
 	private InputStreamReader reader;
-	// Playback data members
 
 	public Client(InetAddress serverAddress, int serverPort, String password) {
 		super("Streaming Protocol Client");
@@ -292,6 +291,16 @@ public class Client extends Thread {
 					socket.send(packetFactory.createPauseMessage(sessionId, isPaused));
 					logger.log(Level.INFO, "Sent Pause Message");
 				}
+				/*
+				else if (input == '+') {
+					socket.send(packetFactory.createThrottleMessage(sessionId, 10240)); // Increase by 10kB/sec
+					logger.log(Level.INFO, "Send Throttle Message, increased rate by 10kB/sec");
+				}
+				else if (input == '-') {
+					socket.send(packetFactory.createThrottleMessage(sessionId, -10240)); // Decrease by 10kB/sec
+					logger.log(Level.INFO, "Send Throttle Message, decreased rate by 10kB/sec");
+				}
+				*/
 			}
 		} catch (IOException e) {
 			logger.log(Level.WARNING, "Error reading from input!", e);
--- protocol/server/src/main/java/edu/drexel/group5/protocol/StreamSession.java
+++ protocol/server/src/main/java/edu/drexel/group5/protocol/StreamSession.java
@@ -47,6 +47,10 @@ public class StreamSession implements Runnable {
 	private final DatagramPacket sessionRequest;
 	private final AudioFormat format;
 	private boolean isPaused = false;
+	private int bytesPerMessage; // Calculated from the audio attributes and the sleep value	
+		
+	// This variable will change to reach the desired rate of bytes/sec
+	private int sleep = 32; // in milliseconds
 
 	/**
 	 *
@@ -206,6 +210,9 @@ public class StreamSession implements Runnable {
 			case PAUSE:
 				handlePauseMessage(packet);
 				break;
+			case THROTTLE:
+				handleThrottleMessage(packet);
+				break;
 		}
 		
 	}
@@ -216,6 +223,23 @@ public class StreamSession implements Runnable {
 		logger.log(Level.WARNING, "Processing Pause Message, sessionId = {0}, paused? = {1}", new Object[]{data[1], data[2]});
 		isPaused = (inputPause == 1);	
 		logger.log(Level.WARNING, "New paused status = ", inputPause);
+	}
+	
+	private void handleThrottleMessage(DatagramPacket packet) {
+		byte[] data = packet.getData();
+		final DataInputStream input = new DataInputStream(new BufferedInputStream(new ByteArrayInputStream(data)));	
+		try {
+			input.skipBytes(2);	
+			int inputRateDiff = input.readInt();
+			logger.log(Level.INFO, "Processing Throttle Message, sessionId = {0}, rate change (bytes/sec) = {1}", new Object[]{data[1], inputRateDiff});
+			int currentRate = (int)(sleep/bytesPerMessage);
+			int newRate = currentRate + inputRateDiff;		
+			sleep = (int)(newRate/bytesPerMessage);
+			logger.log(Level.INFO, "New rate = {0}", newRate);
+			logger.log(Level.INFO, "New calculated sleep time (ms) = {0}", sleep);
+		} catch (IOException ex) {
+			logger.log(Level.WARNING, "Error processing throttle message.", ex);
+		}
 	}
 
 	private void startStreaming() {
@@ -230,9 +254,6 @@ public class StreamSession implements Runnable {
 		private int sequenceNumber = 0;
 		private final MessageDigest digest;
 		private final byte[] buffer;
-		// These two variables control the audio is sent (and therefore the playback quality on the client)
-		private int sleep = 32;
-		private int bytesPerMessage;
 
 		public StreamingThread() {
 			logger.log(Level.INFO, "Streamer thread initialization");
